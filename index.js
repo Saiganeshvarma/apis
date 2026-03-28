6a95d5121307f0ca3d2635557f28d0a1a20c1b76
saiganeshpalla <ganesh@stockholmitacademy.org>
2026-03-28 11:16:18 +0530

    added apis

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+var express = require("express")
+const connectToDatabase = require("./DataBase/db")
+var useRoutes = require("./Routes/userRoutes")
+
+var app = express()
+
+
+app.use(express.json())
+
+app.use("/api/userRoutes",useRoutes)
+
+
+
+
+connectToDatabase()
+
+var port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log("The server is running");
+})
