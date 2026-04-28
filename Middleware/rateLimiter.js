edd095e87813c5c155f7b6caaff21c67407be1cd
saiganeshpalla <ganesh@stockholmitacademy.org>
2026-04-28 11:18:40 +0530

    added

--- Middleware/rateLimiter.js
+++ Middleware/rateLimiter.js
@@ -29,8 +29,8 @@ const createLimiters = () => {
     store: new RedisStore({
       sendCommand: (...args) => client.sendCommand(args),
     }),
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10,
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 1000,
     standardHeaders: true,
     legacyHeaders: false,
     message: { success: false, message: "Too many login attempts, please try again later" },
